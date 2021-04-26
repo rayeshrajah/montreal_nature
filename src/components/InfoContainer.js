851abc1b08af0c8b7ff1891822716ea84fcaa2da
rayeshrajah <rayeshrajah@gmail.com>
2021-04-26 00:31:28 -0400

    added hover effect with grid layout for mini-info-container

--- src/components/InfoContainer.js
+++ src/components/InfoContainer.js
@@ -1,16 +1,18 @@
 import React, { useRef, useLayoutEffect, useState } from "react";
 import "../styles/InfoContainer.css";
 import InfoBox from "../custom_components/InfoBox";
-import Image1 from "../images/mont_royal.jpg"
-import Image2 from "../images/la_maurice.jpg"
-import Image3 from "../images/forillion.jpg"
-import Image4 from "../images/momo_falls.jpg"
+import Image1 from "../images/mont_royal.jpg";
+import Image2 from "../images/la_maurice.jpg";
+import Image3 from "../images/forillion.jpg";
+import Image4 from "../images/momo_falls.jpg";
+import Image5 from "../images/le_bic.jpg";
 
 let arrayTitles = [
                     {title: "Mont-Royal", image: Image1, person:"@jpValery", splashUrl:"https://unsplash.com/@jpvalery"},
                     {title: "La Maurice", image: Image2, person:"@sebastianSeck", splashUrl:"https://unsplash.com/@sebaseck"},
                     {title: "Forillion", image: Image3, person:"@etienneDel", splashUrl:"https://unsplash.com/@etiennedelorieux"},
-                    {title: "Momo Falls", image: Image4, person:"@lukeChesser", splashUrl:"https://unsplash.com/@lukechesser"} 
+                    {title: "Momo Falls", image: Image4, person:"@lukeChesser", splashUrl:"https://unsplash.com/@lukechesser"},
+                    {title: "Le Bic", image: Image5, person:"@alainBon", splashUrl: "https://unsplash.com/@bonnarda"} 
                   ]
 
 export default function InfoContainer() {
